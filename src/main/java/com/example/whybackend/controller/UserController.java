362e83b1a06544e43f611461455010da0910a2b4
amtisyAts <2835908979@qq.com>
2022-07-27 23:22:03 +0800

    comp

--- src/main/java/com/example/whybackend/controller/UserController.java
+++ src/main/java/com/example/whybackend/controller/UserController.java
@@ -1,21 +1,21 @@
-package com.example.whybackend.controller;
-
-import com.example.whybackend.service.impl.HotQuestionImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-@RestController
-public class UserController {
-    @Autowired
-    private HotQuestionImpl hotQuestionService;
-    @ResponseBody
-    @CrossOrigin(origins = "http://localhost:8080",allowCredentials = "true")
-    @RequestMapping(value = "get_hot_question_list", method = RequestMethod.GET)
-    public Object loginStatus() {
-        List res = hotQuestionService.getHotQuestionList();
-        return res;
-    }
-}
+//package com.example.whybackend.controller;
+//
+//import com.example.whybackend.service.impl.HotQuestionImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//import java.util.List;
+//@RestController
+//public class UserController {
+//    @Autowired
+//    private HotQuestionImpl hotQuestionService;
+//    @ResponseBody
+//    @CrossOrigin(origins = "http://localhost:8080",allowCredentials = "true")
+//    @RequestMapping(value = "get_hot_question_list", method = RequestMethod.GET)
+//    public Object loginStatus() {
+//        List res = hotQuestionService.getHotQuestionList();
+//        return res;
+//    }
+//}
